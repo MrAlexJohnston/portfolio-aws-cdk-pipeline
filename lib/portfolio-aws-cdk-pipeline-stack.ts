e0f03b60e29fb56bbc7c2b6bdf6d2f8515f95a1b
Alex Johnston <alex-johnston@live.co.uk>
2024-03-25 15:01:53 +0000

    Adding EsLint to the project

--- lib/portfolio-aws-cdk-pipeline-stack.ts
+++ lib/portfolio-aws-cdk-pipeline-stack.ts
@@ -1,16 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, type StackProps } from 'aws-cdk-lib'
+import { type Construct } from 'constructs'
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines'
 
-export class PortfolioAwsCdkPipelineStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+export class PortfolioAwsCdkPipelineStack extends Stack {
+  constructor (scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'PortfolioAwsCdkPipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'MyPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+      })
+    })
   }
 }
