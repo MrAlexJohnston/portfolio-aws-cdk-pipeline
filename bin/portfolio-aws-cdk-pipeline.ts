38af1040c73c88661c254e25976a45ab12fed472
Alex Johnston <alex-johnston@live.co.uk>
2024-03-25 16:59:52 +0000

    Reshuffle of code pipelines

--- bin/portfolio-aws-cdk-pipeline.ts
+++ bin/portfolio-aws-cdk-pipeline.ts
@@ -1,21 +1,25 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { PortfolioAwsCdkPipelineStack } from '../lib/portfolio-aws-cdk-pipeline-stack';
+import 'source-map-support/register'
+import * as cdk from 'aws-cdk-lib'
+import { PortfolioAwsCdkPipelineStack } from '../lib/portfolio-aws-cdk-pipeline-stack'
+import cdkReposData from '../lib/cdk-repos.json' // Adjust the path as necessary
 
-const app = new cdk.App();
-new PortfolioAwsCdkPipelineStack(app, 'PortfolioAwsCdkPipelineStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+interface RepoInfo {
+  branch: string
+  repo: string
+  owner: string
+}
+const cdkRepos: RepoInfo[] = cdkReposData
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const app = new cdk.App()
+cdkRepos.forEach((repoInfo) => {
+  const { repo } = repoInfo
+  const stackName = restructureRepo(repo)
+  new PortfolioAwsCdkPipelineStack(app, stackName, repoInfo)
+})
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+function restructureRepo (repo: string): string {
+  return repo.split('-')
+    .map(string => string.charAt(0).toUpperCase() + string.slice(1))
+    .join('')
+}
--- lib/portfolio-aws-cdk-pipeline-stack.ts
+++ lib/portfolio-aws-cdk-pipeline-stack.ts
@@ -2,35 +2,30 @@ import { Stack, type StackProps } from 'aws-cdk-lib'
 import { type Construct } from 'constructs'
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines'
 
-import cdkReposData from './cdk-repos.json'
-
 interface RepoInfo {
   branch: string
   repo: string
   owner: string
 }
 
-const cdkRepos: RepoInfo[] = cdkReposData
-
 export class PortfolioAwsCdkPipelineStack extends Stack {
-  constructor (scope: Construct, id: string, props?: StackProps) {
+  constructor (scope: Construct, id: string, repoInfo: RepoInfo, props?: StackProps) {
     super(scope, id, props)
 
-    for (const { branch, repo, owner } of cdkRepos) {
-      const pipelineName = restructureRepo(repo)
-      new CodePipeline(this, pipelineName, {
-        pipelineName,
-        synth: new ShellStep('Synth', {
-          input: CodePipelineSource.gitHub(`${owner}/${repo}`, branch),
-          commands: ['npm ci', 'npm run build', 'npx cdk synth']
-        })
+    const { branch, repo, owner } = repoInfo
+    const pipelineName = this.restructureRepo(repo)
+    new CodePipeline(this, pipelineName, {
+      pipelineName,
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub(`${owner}/${repo}`, branch),
+        commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
-    }
+    })
   }
-}
 
-function restructureRepo (repo: string): string {
-  return repo.split('-')
-    .map(string => string.charAt(0).toUpperCase() + string.slice(1))
-    .join('')
+  private restructureRepo (repo: string): string {
+    return repo.split('-')
+      .map(string => string.charAt(0).toUpperCase() + string.slice(1))
+      .join('')
+  }
 }
