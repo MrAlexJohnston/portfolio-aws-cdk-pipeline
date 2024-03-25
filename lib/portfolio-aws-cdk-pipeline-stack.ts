333641a4d54f2d0df581db3ae9f07edc593f02a6
Alex Johnston <alex-johnston@live.co.uk>
2024-03-25 16:07:21 +0000

    Adding JSON for repos which I wish to deploy

--- lib/portfolio-aws-cdk-pipeline-stack.ts
+++ lib/portfolio-aws-cdk-pipeline-stack.ts
@@ -2,16 +2,35 @@ import { Stack, type StackProps } from 'aws-cdk-lib'
 import { type Construct } from 'constructs'
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines'
 
+import cdkReposData from './cdk-repos.json'
+
+interface RepoInfo {
+  branch: string
+  repo: string
+  owner: string
+}
+
+const cdkRepos: RepoInfo[] = cdkReposData
+
 export class PortfolioAwsCdkPipelineStack extends Stack {
   constructor (scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props)
 
-    new CodePipeline(this, 'Pipeline', {
-      pipelineName: 'MyPipeline',
-      synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
+    for (const { branch, repo, owner } of cdkRepos) {
+      const pipelineName = restructureRepo(repo)
+      new CodePipeline(this, pipelineName, {
+        pipelineName,
+        synth: new ShellStep('Synth', {
+          input: CodePipelineSource.gitHub(`${owner}/${repo}`, branch),
+          commands: ['npm ci', 'npm run build', 'npx cdk synth']
+        })
       })
-    })
+    }
   }
+}
+
+function restructureRepo (repo: string): string {
+  return repo.split('-')
+    .map(string => string.charAt(0).toUpperCase() + string.slice(1))
+    .join('')
 }
