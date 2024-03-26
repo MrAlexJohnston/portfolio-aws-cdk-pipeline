54e4bc5ce7be1bbef402857cc968a47987287c0d
Alex Johnston <alex-johnston@live.co.uk>
2024-03-26 11:53:14 +0000

    Creating custom pipeline

--- bin/portfolio-aws-cdk-pipeline.ts
+++ bin/portfolio-aws-cdk-pipeline.ts
@@ -2,24 +2,6 @@
 import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
 import { PortfolioAwsCdkPipelineStack } from '../lib/portfolio-aws-cdk-pipeline-stack'
-import cdkReposData from '../lib/cdk-repos.json' // Adjust the path as necessary
-
-interface RepoInfo {
-  branch: string
-  repo: string
-  owner: string
-}
-const cdkRepos: RepoInfo[] = cdkReposData
 
 const app = new cdk.App()
-cdkRepos.forEach((repoInfo) => {
-  const { repo } = repoInfo
-  const stackName = restructureRepo(repo)
-  new PortfolioAwsCdkPipelineStack(app, stackName, repoInfo)
-})
-
-function restructureRepo (repo: string): string {
-  return repo.split('-')
-    .map(string => string.charAt(0).toUpperCase() + string.slice(1))
-    .join('')
-}
+new PortfolioAwsCdkPipelineStack(app, 'PortfolioAwsCdkPipelineStack', {})
